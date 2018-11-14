6e42e25699ac6df056751c9370ffc576621e9663
KJ898911 <kshitij898911@gmail.com>
2018-11-14 05:48:27 +0530

    uploading website

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+$("button.btn-lg").on("click" , function(){
+	$("#content").html("<h1>MyProjects.io</h1><h3>A Website featuring my own Projects</h3><hr><button class='btn btn-default btn-lg'><i class='fas fa-drum'></i> <a href = 'PatatapClone.html'>Patatap Clone</a></button><button class='btn btn-default btn-lg'><i class='fas fa-palette'></i> <a href = 'colorgame.html'>Color Game</a></button><button class='btn btn-default btn-lg'><i class='fas fa-clipboard-check'></i> <a href = 'todolist.html'>Todo List</a></button>");
+});
+
+//<a href = 'colorgame.html'>Color Game</a>
+//<a href = 'todolist'>Todo List</a>
